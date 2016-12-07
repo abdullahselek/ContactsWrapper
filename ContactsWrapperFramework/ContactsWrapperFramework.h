2f765fcfd697e18aec6d51159209c7f1a460b773
Abdullah Selek <abdullahselek@gmail.com>
2016-12-07 19:36:03 +0100

    - support carthage

---
+++ ContactsWrapperFramework/ContactsWrapperFramework.h
@@ -0,0 +1,19 @@
+//
+//  ContactsWrapperFramework.h
+//  ContactsWrapperFramework
+//
+//  Created by Abdullah Selek on 07/12/2016.
+//  Copyright © 2016 Abdullah Selek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ContactsWrapperFramework.
+FOUNDATION_EXPORT double ContactsWrapperFrameworkVersionNumber;
+
+//! Project version string for ContactsWrapperFramework.
+FOUNDATION_EXPORT const unsigned char ContactsWrapperFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ContactsWrapperFramework/PublicHeader.h>
+
+
